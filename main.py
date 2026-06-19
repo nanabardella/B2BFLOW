d552d03b63be1630402d46aedbd48eeace02b03f
nanabardella <nanabardella@gmail.com>
2026-06-19 11:58:54 -0300

    feat: configurar conexão com Supabase

--- main.py
+++ main.py
@@ -1,19 +1,50 @@
-# Bibliotecas que utilizei no projeto:
+# Bibliotecas que utilizei no projeto
 import os
 import logging
-import requests
 
 # Carrega as variáveis do arquivo .env
 from dotenv import load_dotenv
+print("URL:", os.getenv("SUPABASE_URL"))
+print("KEY:", os.getenv("SUPABASE_KEY"))
 
-# Importa o cliente do Supabase
+# Cliente oficial do Supabase
 from supabase import create_client, Client
 
 # Carrega as variáveis de ambiente
 load_dotenv()
 
-# Configura os logs exibidos no terminal
+# Configuração dos logs
 logging.basicConfig(
     level=logging.INFO,
     format="%(levelname)s - %(message)s"
 )
+
+# Dados do Supabase vindos do .env
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+
+def create_supabase_client() -> Client:
+    """
+    Cria uma conexão com o Supabase.
+    """
+
+    if not SUPABASE_URL or not SUPABASE_KEY:
+        raise ValueError(
+            "SUPABASE_URL e SUPABASE_KEY são obrigatórios."
+        )
+
+    return create_client(
+        SUPABASE_URL,
+        SUPABASE_KEY
+    )
+
+
+def main():
+    supabase = create_supabase_client()
+
+    print("Conectado ao Supabase com sucesso!")
+
+
+if __name__ == "__main__":
+    main()
